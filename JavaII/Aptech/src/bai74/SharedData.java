2f06d3791f8b0e6608f83cd038c6050cd07087ff
ThanhDuy1310 <45651866+ThanhDuy1310@users.noreply.github.com>
2019-07-01 14:38:53 +0700

    bai74

---
+++ JavaII/Aptech/src/bai74/SharedData.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bai74;
+
+/**
+ *
+ * @author giangduy
+ */
+public class SharedData {
+    int rad;
+
+    public int getRad() {
+        return rad;
+    }
+
+    public void setRad(int rad) {
+        this.rad = rad;
+    }
+
+}
---
+++ JavaII/Aptech/src/bai74/Thread2.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bai74;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+/**
+ *
+ * @author giangduy
+ */
+public class Thread2 extends Thread {
+
+    SharedData sharedData;
+
+    public Thread2(SharedData sharedData) {
+        this.sharedData = sharedData;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Thread2.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        while (true) {
+            synchronized (sharedData) {
+                sharedData.notifyAll();
+
+                try {
+                    sharedData.wait();
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Thread2.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+                int r = sharedData.getRad() * sharedData.getRad();
+                System.out.println("t2: " + r);
+
+            }
+        }
+    }
+
+}
+
